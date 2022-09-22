5ed03b6f8315e75444475272b9e365889f5f516f
ORLANDO DOS SANTOS DOURADO <62121619+OrlandoSD@users.noreply.github.com>
2022-09-22 07:27:25 -0300

    Alteração de diretorios

---
+++ src/main/java/one.digitalinnovation.junit/Conta.java
@@ -0,0 +1,32 @@
+
+package one.digitalinnovation.junit;
+
+import java.util.logging.Logger;
+
+public class Conta {
+
+    private String numeroConta;
+
+    private int saldo;
+
+    public Conta(String numeroConta, int saldo) {
+        this.numeroConta = numeroConta;
+        this.saldo = saldo;
+    }
+
+    public String getNumeroConta() {
+        return numeroConta;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public void lancaCredito(int valor) {
+        this.saldo += valor;
+    }
+
+    public void lancaDebito(int valor) {
+        this.saldo -= valor;
+    }
+}
